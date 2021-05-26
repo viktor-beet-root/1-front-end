869c398e59aae5412f14c3682ffb2a93ba312f5e
Viktor Dehtiarov <4059858@gmail.com>
2021-05-26 22:31:17 +0300

    Js. Lesson 1. Introduction to Javascript

---
+++ main/js/less-1/src/js/script.js
@@ -0,0 +1,27 @@
+// alert('Hello');
+/*
+sdfsdf
+sdfdsf
+*/
+
+// let fullName = "Viktor Dehtiarov";
+// const age = 20;
+// const message = `My full name: ${fullName} i am: ${age}`;
+//"My full name: " + fullName + " i am: " + age;
+// console.log(message);
+
+// console.log(age, fullName, f);
+
+// var f = 22;
+
+// console.log(age, fullName, f);
+
+// // + - * / 
+
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+// const myAge = +prompt("Ваш возраст?");
+// const a = confirm("А вы уверенны?");
+
+// console.log(myAge, a);
+
