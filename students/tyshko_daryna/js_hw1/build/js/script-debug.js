24fdc1c2cc8674ad35a61ec27c8d177b15877763
darynamhappy <daryna.twork@gmail.com>
2021-06-04 09:07:13 +0300

    com1

---
+++ students/tyshko_daryna/js_hw1/build/js/script-debug.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var name = +prompt("Введіть ваше ім`я", "");
+document.write("Привіт!", "");
+console.log(write); // var userName = prompt(" Введите своё имя ");
+// var hello = " Привет, ";
---
+++ students/tyshko_daryna/js_hw1/src/js/script.js
@@ -0,0 +1,7 @@
+var name = +prompt("Введіть ваше ім`я", "");
+document.write("Привіт!", "");
+
+console.log(write);
+
+// var userName = prompt(" Введите своё имя ");
+// var hello = " Привет, ";
