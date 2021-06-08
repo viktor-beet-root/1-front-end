dc4c5d93ae9d2c7bae71869dc767a96c81af7f95
Alina Zhechkova <alinazhech@gmail.com>
2021-06-08 15:40:26 +0300

    finished 2 tasks else

--- students/zhechkova_alina/hw13/js/script.js
+++ students/zhechkova_alina/hw13/js/script.js
@@ -1,3 +1,4 @@
+
 /*// Первая задача
 const num = +prompt('Введите число');
 let i = 0;
@@ -36,21 +37,28 @@ while (i++ < number) {
     if (number % i === 0) console.log(i);
 }
 
+
 // Запросить 2 числа и найти только наибольший общий делитель
 
 let firstNum = +prompt('Первое число');
 let secondNum = +prompt('Второе число');
 let i = 0;
+let minNumber = firstNum < secondNum ? firstNum : secondNum;
+let maxNumber = firstNum > secondNum ? firstNum : secondNum;
 let maxDelitel = 0;
-while (i++ < firstNum || i++ < secondNum) { console.log(i)
+while (i++ < minNumber) {
+    console.log(i)
+    if (maxNumber % minNumber === 0) {
+        maxDelitel = minNumber;
+        break;
+    }
     if (firstNum % i === 0 && secondNum % i === 0) maxDelitel = i;
 }
-console.log(maxDelitel)
-*/
+console.log(maxDelitel);
 
 // Определить количество цифр в введенном числе.
 const num1 = +prompt('Введите число');
-let i = 1;
+ i = 1;
 let r = Math.floor(num1 / 10);
 while (r > 0) {
     i++;
@@ -59,7 +67,6 @@ while (r > 0) {
 console.log(i);
 
 
-/*
 // Запросить у пользователя 10 чисел и подсчитать, сколько он ввел положительных, отрицательных и нулей .При этом также посчитать, сколько четных и нечетных. Вывести статистику на экран.
 
 let tenNum;
@@ -106,9 +113,26 @@ do {
     question = confirm('Хотите ли решить еще один пример?');
 } while (question == true);
 
+
 // Запросить у пользователя число и на сколько цифр его сдвинуть.Сдвинуть цифры числа и вывести результат(если число 123456 сдвинуть на 2 цифры, то получится 345612).
-const num3 = +prompt('Введите число');
+let num3 = +prompt('Введите число');
 let numChange = +prompt('На сколько цифр сдвинуть?');
+let long = 1;
+let amountRegist = Math.floor(num3 / 10);
+let numLast = 0;
+while (amountRegist > 0) {
+    long++;
+    amountRegist = Math.floor(amountRegist / 10);
+}
+
+for (let i = 0; i < numChange; i++) {
+    numLast = num3;
+    for (let j = 0; j < long - 1; j++) {
+        numLast = Math.floor(numLast / 10);
+    }
+    num3 = num3 % Math.pow(10, long-1)*10 + numLast; 
+}
+console.log(num3)
 
 
 // Зациклить вывод дней недели таким образом: «День недели.Хотите увидеть следующий день ? »и так до тех пор, пока пользователь нажимает OK. 
@@ -137,7 +161,7 @@ for (let i = 2; i < 10; i++) {
     }
 }
 console.log(multiply);
-*/
+
 
 /*  Игра« Угадай число».Предложить пользователю загадать число от 0 до 100 и отгадать его следующим способом:
              каждую итерацию цикла делите диапазон чисел пополам,
@@ -145,15 +169,28 @@ console.log(multiply);
              .В зависимости от того, что указал пользователь, уменьшаете диапазон.Начальный диапазон от 0 до 100,
              поделили пополам и получили 50. Если пользователь указал, что его число > 50,
              то изменили диапазон на от 51 до 100. И так до тех пор, пока пользователь не выберет == N. 
-
-
-let border = 100;
-let i = 2;
-const numGuess = +prompt(`Загадайте число от 0 до ${border}`)
-let expected = prompt(`Ваше число \n 1) < ${border / i}, \n 2) > ${border / i} \n или 3) = ${border / i}`);
-let result = '';
-while (i < 16) {
-    i += 2;
-}
-alert(result);
 */
+let startDiap = 0;
+let finishDiap = 100;
+const numGuess = +prompt(`Загадайте число от 0 до 100`);
+let result = 0;
+let i = 0;
+let ask;
+let expected;
+while (i < 10) {
+    i+=2;
+    ask = (startDiap + finishDiap) / i;
+    expected = prompt(`1) > ${ask} \n2) < ${ask} \n3) == ${ask}`)
+    if (expected === '3)') {
+        result = ask;
+        break;
+    }
+    if (expected === '1)') {
+        startDiap = finishDiap / 2;
+    }
+    if (expected === '2)') {
+        finishDiap = finishDiap / 2;
+    }
+}
+
+alert(result);
