a5311938ec416a4147bb3c595044243df4637917
darynamhappy <daryna.twork@gmail.com>
2021-06-11 09:26:20 +0300

    com1

--- students/tyshko_daryna/js_hw1/src/js/script.js
+++ students/tyshko_daryna/js_hw1/src/js/script.js
@@ -1,7 +1,28 @@
-var name = +prompt("Введіть ваше ім`я", "");
-document.write("Привіт!", "");
+//1)   Запросите у пользователя его имя и выведите в ответ: «Привет, его имя!».
 
-console.log(write);
+var yourName = prompt('Ваше ім`я');
+var messageName = "Привіт, " + yourName + ".";
 
-// var userName = prompt(" Введите своё имя ");
-// var hello = " Привет, ";
+console.log(messageName); 
+
+ //  2)   Запросите у пользователя год его рождения, посчитайте, сколько ему лет и выведите результат. Текущий год укажите в коде как константу.
+ var thisYear = 2021;
+ var userYear = +prompt('Введіть свій рік нaродження');
+ var massageYear = "Вам " + (thisYear - userYear) + " роки.";
+
+ console.log(massageYear); 
+
+// 3)   Запросите у пользователя длину стороны квадрата и выведите периметр такого квадрата.
+ var sideSquare = prompt('Введіть довжину сторони квадрата');
+ var squarePerimeter = 4 * sideSquare;
+ var count = "Периметр квадрата = " + squarePerimeter;
+
+ console.log(count);
+
+//  4)   Запросите у пользователя радиус окружности и выведите площадь такой окружности.
+var numberPi = 3.14;
+var radius = prompt('Введіть радіус');
+
+
+
+
