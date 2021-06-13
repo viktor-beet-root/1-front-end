b89a6dded93fe22888eab8795180770d1dd631df
Oleksii-oun <gugen.c4@gmail.com>
2021-06-13 18:35:06 +0300

    rename let and add task 10

--- students/oleksii_kovalenko/js_hw_2/build/js/script-debug.js
+++ students/oleksii_kovalenko/js_hw_2/build/js/script-debug.js
@@ -1,167 +1,162 @@
 "use strict";
 
 // 1.Запросить у пользователя его возраст и определить, кем он является: ребенком (0–12), подростком (12–18), взрослым (18_60) или пенсионером (60– ...).
-var yourAge = +prompt("Введите ваш возраст");
-var messageAge;
+
+/*
+const yourAge = +prompt ("Введите ваш возраст");
+let messageAge;
 
 if (Number.isNaN(yourAge)) {
-  messageAge = "Введите число";
-} else if (0 <= yourAge && yourAge <= 12) {
-  messageAge = "Вы ребенок";
-} else if (12 <= yourAge && yourAge <= 18) {
-  messageAge = "Вы подросток";
-} else if (18 <= yourAge && yourAge <= 60) {
-  messageAge = "Вы взрослый";
-} else if (60 <= yourAge && yourAge <= 120) {
-  messageAge = "Вы пенсионер";
+    messageAge = "Введите число";
+} else if ( yourAge => 0  && yourAge <=12) {
+    messageAge = "Вы ребенок";
+} else if ( yourAge > 12 && yourAge <= 18) {
+    messageAge = "Вы подросток";
+} else if ( yourAge > 18  && yourAge <= 60) {
+    messageAge = "Вы взрослый";
+} else if ( yourAge > 60  && yourAge <= 120) {
+    messageAge = "Вы пенсионер";
 } else {
-  whoAreYou = 'not corect';
+  whoAreYou = 'Not corect';
 }
 
-console.log(messageAge); 
+console.log(messageAge);
 
 //2.Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д).
 
-var keyNumber = +prompt("Введите число от 0 до 9");
-var messageKeySimbol;
+const keyNumber = +prompt ("Введите число от 0 до 9");
+let messageKeySimbol;
 
 switch (keyNumber) {
-  case 1:
-    messageKeySimbol = "!";
-    break;
-
-  case 2:
-    messageKeySimbol = "@";
-    break;
-
-  case 3:
-    messageKeySimbol = "#";
-    break;
-
-  case 4:
-    messageKeySimbol = "$";
-    break;
-
-  case 5:
-    messageKeySimbol = "%";
-    break;
-
-  case 6:
-    messageKeySimbol = "^";
-    break;
-
-  case 7:
-    messageKeySimbol = "&";
-    break;
-
-  case 8:
-    messageKeySimbol = "*";
-    break;
-
-  case 9:
-    messageKeySimbol = "(";
-    break;
-
-  case 0:
-    messageKeySimbol = ")";
-    break;
-
-  default:
-    messageKeySimbol = "No correct";
+    case 1:
+        messageKeySimbol = "!"
+        break;
+    case 2:
+        messageKeySimbol = "@"
+        break;
+    case 3:
+        messageKeySimbol = "#"
+        break;
+    case 4:
+        messageKeySimbol = "$"
+        break;
+    case 5:
+        messageKeySimbol = "%"
+        break;
+    case 6:
+        messageKeySimbol = "^"
+        break;
+    case 7:
+        messageKeySimbol = "&"
+        break;
+    case 8:
+        messageKeySimbol = "*"
+        break;
+    case 9:
+        messageKeySimbol = "("
+        break;
+    case 0:
+        messageKeySimbol = ")"
+        break;
+    default :
+        messageKeySimbol = "No correct";
 }
 
-console.log(messageKeySimbol); 
+console.log(messageKeySimbol);
 
 // 3.Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры.!!!!!!
 
-var numbers = +prompt("введите 3-х значное число от 100 до 999");
-var number1 = ~~(numbers / 100);
-var number2 = ~~(numbers / 10) % 10;
-var number3 = numbers % 10;
-var numbersMessage;
+const numbers = +prompt("введите 3-х значное число от 100 до 999");
+const number1 = ~~(numbers / 100);
+const number2 = ~~(numbers / 10) % 10 ;
+const number3 = numbers % 10;
+let numbersMessage;
 
 if (Number.isNaN(numbers)) {
-  numbersMessage = "Введите число!";
+    numbersMessage = "Введите число!";
 } else if (number1 === number2 || number2 === number3 || number3 === number1) {
-  numbersMessage = "Совпадение чисел найдено";
+    numbersMessage = "Совпадение чисел найдено";
 } else {
-  numbersMessage = "Совпадение чисел не найдено";
+    numbersMessage = "Совпадение чисел не найдено";
 }
 
 console.log(numbersMessage);
 
 // 4.Запросить у пользователя год и проверить, високосный он или нет. Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100.
 
-var year = +prompt("Введите год");
-var yearMessage;
+const year = +prompt ("Введите год");
+let yearMessage;
 
 if (Number.isNaN(year)) {
-  yearMessage = "Введите число";
-} else if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
-  yearMessage = "Год высокосный";
+    yearMessage ="Введите число";
+} else if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    yearMessage = "Год высокосный";
 } else {
-  yearMessage = "Год не высокосный";
+    yearMessage = "Год не высокосный";
 }
-
 console.log(yearMessage);
 
 //5. Запросить у пользователя пятиразрядное число и определить, является ли оно палиндромом.
 
-var palidromeNumber = +prompt('Введите число от 10000 до 99999');
-var digit1 = Math.floor(palidromeNumber / 10000 % 10);
-var digit2 = Math.floor(palidromeNumber / 1000 % 10);
-var digit3 = Math.floor(palidromeNumber / 10 % 10);
-var digit4 = Math.floor(palidromeNumber / 1 % 10);
-var palidromeAnswer;
+const palidromeNumber = +prompt('Введите число от 10000 до 99999');
+const digit1 = Math.floor(palidromeNumber / 10000 % 10);
+const digit2 = Math.floor(palidromeNumber / 1000 % 10);
+const digit3 = Math.floor(palidromeNumber / 10 % 10);
+const digit4 = Math.floor(palidromeNumber / 1 % 10);
+let palidromeAnswer;
 
 if (Number.isNaN(palidromeNumber)) {
-  palidromeAnswer = 'Введите число!';
+    palidromeAnswer = 'Введите число!';
 } else if (digit1 === digit4 && digit2 === digit3) {
-  palidromeAnswer = 'Палидром';
+    palidromeAnswer ='Палидром';
 } else {
-  palidromeAnswer = 'Не палидром';
+    palidromeAnswer = 'Не палидром';
 }
 
 console.log(palidromeAnswer);
 
+
 //6. Написать конвертор валют. Пользователь вводит количество USD, выбирает, в какую валюту хочет перевести EUR, UAN или AZN, и получает в ответ соответствующую сумму.
 
-var amount = +prompt('Введите сумму USD');
-var currency = +prompt('EUR = 1 , UAN = 2 , AZN = 3');
-var messageAmount;
+const amount = +prompt('Введите сумму USD');
+const currency = +prompt( 'EUR = 1 , UAN = 2 , AZN = 3');
+const euro = 0.82;
+const hrivna = 27.38;
+const azerbaijaniManat = 1.70;
+let messageAmount;
 
-switch (currency) {
-  case 1:
-    messageAmount = amount * 0.82 + ' EUR';
-    break;
-
-  case 2:
-    messageAmount = amount * 27.38 + ' UAN';
-    break;
-
-  case 3:
-    messageAmount = amount * 1.70 + ' AZN';
-    break;
-}
-
-console.log(messageAmount); 
-
+    switch(currency){
+        case 1:
+            messageAmount = amount * euro + ' EUR';
+            break;
+        case 2:
+            messageAmount = amount * hrivna + ' UAN';
+            break;
+        case 3:
+             messageAmount = amount * azerbaijaniManat + ' AZN';
+            break;
+    }
+console.log(messageAmount);
+*/
 //7.Запросить у пользователя сумму покупки и вывести сумму к оплате со скидкой: от 200 до 300 – скидка будет 3%, от 300 до 500 – 5%, от 500 и выше – 7%.
 
 var purchaseValue = +prompt('Введите сумму покупки');
+var threePercent = 0.97;
+var fivePercent = 0.95;
+var sevenPercent = 0.93;
 var purchaseValueDiscaunt;
 
 if (purchaseValue >= 200 && purchaseValue <= 300) {
-  purchaseValueDiscaunt = purchaseValue / 100 * 97 + ' со скидной 3%';
+  purchaseValueDiscaunt = purchaseValue * threePercent + ' Цена со скидной 3%';
 } else if (purchaseValue >= 300 && purchaseValue <= 500) {
-  purchaseValueDiscaunt = purchaseValue / 100 * 95 + ' со скидной 5%';
+  purchaseValueDiscaunt = purchaseValue * fivePercent + ' Цена со скидной 5%';
 } else if (purchaseValue >= 500) {
-  purchaseValueDiscaunt = purchaseValue / 100 * 93 + ' со скидной 7%';
+  purchaseValueDiscaunt = purchaseValue * sevenPercent + ' Цена со скидной 7%';
 } else if (Number.isNaN(purchaseValue)) {
   purchaseValueDiscaunt = 'Введите число';
 }
 
-console.log(purchaseValueDiscaunt); 
+console.log(purchaseValueDiscaunt);
 
 //8.Запросить у пользователя длину окружности и периметр квадрата. Определить, может ли такая окружность поместиться в указанный квадрат.
 
@@ -179,7 +174,7 @@ if (Number.isNaN(circumference && perimeterSquare)) {
   possibly = 'Не поместится';
 }
 
-console.log(possibly);
+console.log(possibly); 
 
 //9.Задать пользователю 3 вопроса, в каждом вопросе по 3 варианта ответа. За каждый правильный ответ начисляется 2 балла. После вопросов выведите пользователю количество набранных баллов.
 
@@ -190,68 +185,39 @@ var score = 0;
 if (river === 2) score += 2;
 if (president === 1) score += 2;
 if (capital === 1) score += 2;
-console.log('Вы набрали ' + score + ' баллов'); 
+console.log('Вы набрали ' + score + ' баллов');
 
 //10.Запросить дату (день, месяц, год) и вывести следующую за ней дату. Учтите возможность перехода на следующий месяц, год, а также високосный год.
 
-var a = +prompt('Введите год');
-var b = +prompt('Янвать = 0 , Февраль = 1 , Март = 2, Аперль = 3, Май = 4, Июнь = 5,  Июль = 6, Август = 7, Сентябрь = 8, Октябрь = 9, Ноябрь = 10, Дкабрь = 11');
-var c = +prompt('Введите день');
+var Day = +prompt('Введите день', '');
+var Month = +prompt('Введите месяц', '');
+var Year = +prompt('Введите год', '');
 
-switch (b) {
-  case 0:
-    myNewDate;
-    break;
-
-  case 1:
-    myNewDate;
-    break;
-
-  case 2:
-    myNewDate;
-    break;
-
-  case 3:
-    myNewDate;
-    break;
-
-  case 4:
-    myNewDate;
-    break;
-
-  case 5:
-    myNewDate;
-    break;
-
-  case 6:
-    myNewDate;
-    break;
-
-  case 7:
-    myNewDate;
-    break;
-
-  case 8:
-    mmyNewDate;
-    break;
-
-  case 9:
-    myNewDate;
-    break;
-
-  case 10:
-    myNewDate;
-    break;
-
-  case 11:
-    myNewDate;
-    break;
-
-  default:
-    myNewDate = "No correct";
+if (Day === 32) {
+  console.log("Больше 32 дней многовато");
+} else if (Month === 13) {
+  console.log("Больше 13 месяцев");
+} else if (Day < 29 && Month === 2 && (Year % 400 === 0 || Year % 4 === 0 && Year % 100 !== 0)) {
+  Day++;
+  console.log('День: ' + Day, 'Месяц: ' + Month, 'Год: ' + Year);
+} else if (Day > 29 && Month === 2) {
+  Day = 1;
+  Month++;
+  console.log('День: ' + Day, 'Месяц: ' + Month, 'Год: ' + Year);
+} else if (Day === 28 && Month === 2) {
+  Day = 1;
+  Month++;
+  console.log('День: ' + Day, 'Месяц: ' + Month, 'Год: ' + Year);
+} else if (Day === 31 && Month === 12) {
+  Day = 1;
+  Month = 1;
+  Year++;
+  console.log('День: ' + Day, 'Месяц: ' + Month, 'Год: ' + Year);
+} else if (Day === 31 && Month < 12) {
+  Day = 1;
+  Month++;
+  console.log('День: ' + Day, 'Месяц: ' + Month, 'Год: ' + Year);
+} else {
+  Day++;
+  console.log('День: ' + Day, 'Месяц: ' + Month, 'Год: ' + Year);
 }
-
-var theDate = new Date(a, b, c);
-var myNewDate = new Date(theDate);
-myNewDate.setDate(myNewDate.getDate() + 1);
-console.log(myNewDate);
--- students/oleksii_kovalenko/js_hw_2/src/js/script.js
+++ students/oleksii_kovalenko/js_hw_2/src/js/script.js
@@ -1,27 +1,27 @@
 // 1.Запросить у пользователя его возраст и определить, кем он является: ребенком (0–12), подростком (12–18), взрослым (18_60) или пенсионером (60– ...).
-
-let yourAge = +prompt ("Введите ваш возраст");
+/*
+const yourAge = +prompt ("Введите ваш возраст");
 let messageAge;
 
 if (Number.isNaN(yourAge)) {
     messageAge = "Введите число";
-} else if ( 0 <= yourAge && yourAge <=12) {
+} else if ( yourAge => 0  && yourAge <=12) {
     messageAge = "Вы ребенок";
-} else if ( 12 <= yourAge && yourAge <= 18) {
+} else if ( yourAge > 12 && yourAge <= 18) {
     messageAge = "Вы подросток";
-} else if (18 <= yourAge && yourAge <= 60) {
+} else if ( yourAge > 18  && yourAge <= 60) {
     messageAge = "Вы взрослый";
-} else if (60 <= yourAge && yourAge <= 120) {
+} else if ( yourAge > 60  && yourAge <= 120) {
     messageAge = "Вы пенсионер";
 } else {
-  whoAreYou = 'not corect';
+  whoAreYou = 'Not corect';
 }
 
 console.log(messageAge);
 
 //2.Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д).
 
-let keyNumber = +prompt ("Введите число от 0 до 9");
+const keyNumber = +prompt ("Введите число от 0 до 9");
 let messageKeySimbol;
 
 switch (keyNumber) {
@@ -63,10 +63,10 @@ console.log(messageKeySimbol);
 
 // 3.Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры.!!!!!!
 
-let numbers = +prompt("введите 3-х значное число от 100 до 999");
-let number1 = ~~(numbers / 100);
-let number2 = ~~(numbers / 10) % 10 ;
-let number3 = numbers % 10;
+const numbers = +prompt("введите 3-х значное число от 100 до 999");
+const number1 = ~~(numbers / 100);
+const number2 = ~~(numbers / 10) % 10 ;
+const number3 = numbers % 10;
 let numbersMessage;
 
 if (Number.isNaN(numbers)) {
@@ -81,12 +81,12 @@ console.log(numbersMessage);
 
 // 4.Запросить у пользователя год и проверить, високосный он или нет. Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100.
 
-let year = +prompt ("Введите год");
+const year = +prompt ("Введите год");
 let yearMessage;
 
 if (Number.isNaN(year)) {
     yearMessage ="Введите число";
-} else if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+} else if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
     yearMessage = "Год высокосный";
 } else {
     yearMessage = "Год не высокосный";
@@ -95,11 +95,11 @@ console.log(yearMessage);
 
 //5. Запросить у пользователя пятиразрядное число и определить, является ли оно палиндромом.
 
-let palidromeNumber = +prompt('Введите число от 10000 до 99999');
-let digit1 = Math.floor(palidromeNumber / 10000 % 10);
-let digit2 = Math.floor(palidromeNumber / 1000 % 10);
-let digit3 = Math.floor(palidromeNumber / 10 % 10);
-let digit4 = Math.floor(palidromeNumber / 1 % 10);
+const palidromeNumber = +prompt('Введите число от 10000 до 99999');
+const digit1 = Math.floor(palidromeNumber / 10000 % 10);
+const digit2 = Math.floor(palidromeNumber / 1000 % 10);
+const digit3 = Math.floor(palidromeNumber / 10 % 10);
+const digit4 = Math.floor(palidromeNumber / 1 % 10);
 let palidromeAnswer;
 
 if (Number.isNaN(palidromeNumber)) {
@@ -115,34 +115,40 @@ console.log(palidromeAnswer);
 
 //6. Написать конвертор валют. Пользователь вводит количество USD, выбирает, в какую валюту хочет перевести EUR, UAN или AZN, и получает в ответ соответствующую сумму.
 
-let amount = +prompt('Введите сумму USD');
-let currency = +prompt( 'EUR = 1 , UAN = 2 , AZN = 3');
+const amount = +prompt('Введите сумму USD');
+const currency = +prompt( 'EUR = 1 , UAN = 2 , AZN = 3');
+const euro = 0.82;
+const hrivna = 27.38;
+const azerbaijaniManat = 1.70;
 let messageAmount;
 
     switch(currency){
         case 1:
-            messageAmount = amount * 0.82 + ' EUR';
+            messageAmount = amount * euro + ' EUR';
             break;
         case 2:
-            messageAmount = amount * 27.38 + ' UAN';
+            messageAmount = amount * hrivna + ' UAN';
             break;
         case 3:
-             messageAmount = amount * 1.70 + ' AZN';
+             messageAmount = amount * azerbaijaniManat + ' AZN';
             break;
     }
 console.log(messageAmount);
-
+*/
 //7.Запросить у пользователя сумму покупки и вывести сумму к оплате со скидкой: от 200 до 300 – скидка будет 3%, от 300 до 500 – 5%, от 500 и выше – 7%.
 
-let purchaseValue = +prompt ('Введите сумму покупки');
+const purchaseValue = +prompt ('Введите сумму покупки');
+const threePercent = 0.97;
+const fivePercent = 0.95;
+const sevenPercent = 0.93;
 let purchaseValueDiscaunt;
 
 if (purchaseValue >= 200 && purchaseValue <= 300) {
-    purchaseValueDiscaunt = (purchaseValue / 100 * 97) + ' со скидной 3%';
+    purchaseValueDiscaunt = purchaseValue * threePercent  + ' Цена со скидной 3%';
 } else if (purchaseValue >= 300 && purchaseValue <= 500) {
-    purchaseValueDiscaunt = (purchaseValue / 100 * 95) + ' со скидной 5%';
+    purchaseValueDiscaunt = purchaseValue * fivePercent + ' Цена со скидной 5%';
 } else if (purchaseValue >= 500) {
-    purchaseValueDiscaunt = (purchaseValue / 100 * 93) + ' со скидной 7%';
+    purchaseValueDiscaunt = purchaseValue * sevenPercent + ' Цена со скидной 7%';
 } else if  (Number.isNaN(purchaseValue)) {
     purchaseValueDiscaunt = 'Введите число';
 }
@@ -151,8 +157,8 @@ console.log(purchaseValueDiscaunt);
 
 //8.Запросить у пользователя длину окружности и периметр квадрата. Определить, может ли такая окружность поместиться в указанный квадрат.
 
-let circumference = +prompt ('Введите длину окружности');
-let perimeterSquare = +prompt ('Введите периметр квадрата');
+const circumference = +prompt ('Введите длину окружности');
+const perimeterSquare = +prompt ('Введите периметр квадрата');
 let diametr = circumference / Math.PI;
 let perimeter = perimeterSquare / 4;
 let possibly;
@@ -183,53 +189,38 @@ console.log('Вы набрали ' + score +' баллов');
 
 //10.Запросить дату (день, месяц, год) и вывести следующую за ней дату. Учтите возможность перехода на следующий месяц, год, а также високосный год.
 
-
-let a = +prompt('Введите год');
-let b = +prompt('Янвать = 0 , Февраль = 1 , Март = 2, Аперль = 3, Май = 4, Июнь = 5,  Июль = 6, Август = 7, Сентябрь = 8, Октябрь = 9, Ноябрь = 10, Дкабрь = 11');
-let c = +prompt('Введите день');
-
-switch (b) {
-    case 0:
-        myNewDate;
-        break;
-    case 1:
-        myNewDate;
-        break;
-    case 2:
-        myNewDate;
-        break;
-    case 3:
-        myNewDate;
-        break;
-    case 4:
-        myNewDate;
-        break;
-    case 5:
-        myNewDate;
-        break;
-    case 6:
-        myNewDate;
-        break;
-    case 7:
-        myNewDate;
-        break;
-    case 8:
-        mmyNewDate;
-        break;
-    case 9:
-        myNewDate;
-        break;
-    case 10:
-        myNewDate;
-        break;
-    case 11:
-        myNewDate;
-        break;
-    default :
-    myNewDate = "No correct";
+let Day = +prompt('Введите день', '');
+let Month = +prompt('Введите месяц', '');
+let Year = +prompt('Введите год', '');
+if (Day === 32) {
+	console.log("Больше 32 дней многовато");
+} else if (Month === 13) {
+	console.log("Больше 13 месяцев");
+} else if (
+	Day < 29 &&
+	Month === 2 &&
+	(Year % 400 === 0 || (Year % 4 === 0 && Year % 100 !== 0))
+) {
+	Day++;
+	console.log('День: ' + Day, 'Месяц: '+ Month, 'Год: ' + Year);
+} else if (Day > 29 && Month === 2) {
+	Day = 1;
+	Month++;
+	console.log('День: ' + Day, 'Месяц: '+ Month, 'Год: ' + Year);
+} else if (Day === 28 && Month === 2) {
+	Day = 1;
+	Month++;
+	console.log('День: ' + Day, 'Месяц: '+ Month, 'Год: ' + Year);
+} else if (Day === 31 && Month === 12) {
+	Day = 1;
+	Month = 1;
+	Year++;
+	console.log('День: ' + Day, 'Месяц: '+ Month, 'Год: ' + Year);
+} else if (Day === 31 && Month < 12) {
+	Day = 1;
+	Month++;
+	console.log('День: ' + Day, 'Месяц: '+ Month, 'Год: ' + Year);
+} else {
+	Day++;
+	console.log('День: ' + Day, 'Месяц: '+ Month, 'Год: ' + Year);
 }
-
-let theDate = new Date(a, b, c);
-let myNewDate = new Date(theDate);
-myNewDate.setDate(myNewDate.getDate() + 1);
-console.log(myNewDate);
