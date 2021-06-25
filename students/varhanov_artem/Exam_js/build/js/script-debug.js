3ebf9bb35e07e67120bcba08a20f4bddafc7038a
artemvarhanov <art.varganov@gmail.com>
2021-06-25 10:38:11 +0300

    add section What we do

---
+++ students/varhanov_artem/Exam_js/build/js/script-debug.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$('.hero').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
---
+++ students/varhanov_artem/Exam_js/src/js/script.js
@@ -0,0 +1,8 @@
+$('.hero').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
