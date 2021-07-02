5f067b7b663f3f8b020a3226a45026ea44e813e0
artemvarhanov <art.varganov@gmail.com>
2021-07-02 18:41:27 +0300

    LATEST NEWS

--- students/varhanov_artem/Exam_js/build/js/script-debug.js
+++ students/varhanov_artem/Exam_js/build/js/script-debug.js
@@ -6,4 +6,26 @@ $('.hero').slick({
   speed: 500,
   fade: true,
   cssEase: 'linear'
-});
+}); // $('.news__row').slick({
+//   dots: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 4000,
+//   responsive: [
+//     {
+//       breakpoint: 1000,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// });
--- students/varhanov_artem/Exam_js/src/js/script.js
+++ students/varhanov_artem/Exam_js/src/js/script.js
@@ -6,3 +6,30 @@ $('.hero').slick({
     cssEase: 'linear'
   });
 
+
+ 
+  // $('.news__row').slick({
+  //   dots: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 4000,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1000,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+     
+  //   ]
+  // });
+    
