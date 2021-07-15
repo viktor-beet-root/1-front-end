fef1d23a1d571a08f8758ef1a7fb51c378a11191
artemvarhanov <art.varganov@gmail.com>
2021-07-15 17:15:57 +0300

    finish

--- students/varhanov_artem/Exam_js/build/js/script-debug.js
+++ students/varhanov_artem/Exam_js/build/js/script-debug.js
@@ -1,31 +1,76 @@
 "use strict";
 
-$('.hero').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-}); // $('.news__row').slick({
-//   dots: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   autoplaySpeed: 4000,
-//   responsive: [
-//     {
-//       breakpoint: 1000,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
-//   ]
-// });
+$(document).ready(function () {
+  $('.hero').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $('.news__row').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [{
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+  $("#scroll").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+  $(".mobile-menu__menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+  $('.button-mobmenu').click(function (event) {
+    $('.button-mobmenu,.mobile-menu').toggleClass("active");
+  });
+});
+
+function initMap() {
+  var brooklun = {
+    lat: 40.686280255564796,
+    lng: -73.90061989110093
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: brooklun,
+    zoom: 13
+  });
+  var marker = new google.maps.Marker({
+    position: brooklun,
+    map: map,
+    title: 'Park',
+    icon: 'image/Pin.png'
+  });
+}
--- students/varhanov_artem/Exam_js/src/js/script.js
+++ students/varhanov_artem/Exam_js/src/js/script.js
@@ -1,4 +1,5 @@
-$('.hero').slick({
+$(document).ready(function(){
+  $('.hero').slick({
     dots: true,
     infinite: true,
     speed: 500,
@@ -6,30 +7,71 @@ $('.hero').slick({
     cssEase: 'linear'
   });
 
-
- 
-  // $('.news__row').slick({
-  //   dots: true,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 4000,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1000,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     }
+  $('.news__row').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          arrows:false,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
      
-  //   ]
-  // });
+    ]
+  });
+  
+
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        const id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+  $("#scroll").on("click", function (event) {
+      event.preventDefault();
+      const id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+  $(".mobile-menu__menu").on("click","a", function (event) {
+      event.preventDefault();
+      const id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  }); 
+
+  $('.button-mobmenu').click(function(event){
+$('.button-mobmenu,.mobile-menu').toggleClass("active")
+  });
+});
+
+function initMap() {
+  const brooklun = {lat: 40.686280255564796, lng: -73.90061989110093};
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: brooklun,
+    zoom: 13
+  });
+
+  const marker = new google.maps.Marker({
+    position: brooklun,
+    map,
+    title:'Park',
+    icon:'image/Pin.png'
     
+  });
+}
