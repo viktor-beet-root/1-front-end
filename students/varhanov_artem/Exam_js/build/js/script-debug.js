36bf7852e1cff5bf0cbb93499bed0b343e616965
artemvarhanov <art.varganov@gmail.com>
2021-07-23 16:31:46 +0300

    add some fif

--- students/varhanov_artem/Exam_js/build/js/script-debug.js
+++ students/varhanov_artem/Exam_js/build/js/script-debug.js
@@ -1,5 +1,34 @@
 "use strict";
 
+function throttle(func, ms) {
+  var isThrottled = false,
+      savedArgs,
+      savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+    setTimeout(function () {
+      isThrottled = false; // (3)
+
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
 $(document).ready(function () {
   $('.hero').slick({
     dots: true,
@@ -56,6 +85,31 @@ $(document).ready(function () {
   $('.button-mobmenu').click(function (event) {
     $('.button-mobmenu,.mobile-menu').toggleClass("active");
   });
+}); // $(window).scroll(function(){
+//   if ($(window).scrollTop() > 300) {
+//       $('.header').addClass('sticky');
+//   }
+//   else {
+//       $('.header').removeClass('sticky');
+//   }
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var hero = document.querySelector('.hero');
+  var header = document.querySelector('.header');
+  var heroCenter = hero.offsetHeight / 2; // const showHeaderThrottle = throttle(() => {
+  //   const scrollTop = window.scrollY;
+  //   (scrollTop >= 100) ? header.classList.add('invisible') : header.classList.remove('invisible');
+  //   (scrollTop >= heroCenter) ? header.classList.add('sticky') : header.classList.remove('sticky');
+  // }, 500)
+
+  var showHeaderThrottle = function showHeaderThrottle() {
+    var scrollTop = window.scrollY;
+    scrollTop >= 100 ? header.classList.add('invisible') : header.classList.remove('invisible');
+    scrollTop >= heroCenter ? header.classList.add('sticky') : header.classList.remove('sticky');
+  };
+
+  window.addEventListener('scroll', showHeaderThrottle);
 });
 
 function initMap() {
@@ -65,7 +119,19 @@ function initMap() {
   };
   var map = new google.maps.Map(document.getElementById('map'), {
     center: brooklun,
-    zoom: 13
+    zoom: 13,
+    styles: [{
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#e8e8e8"
+      }]
+    }, {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#757575"
+      }]
+    }]
   });
   var marker = new google.maps.Marker({
     position: brooklun,
@@ -74,3 +140,24 @@ function initMap() {
     icon: 'image/Pin.png'
   });
 }
+
+var sections = document.querySelectorAll("section");
+var navLi = document.querySelectorAll("nav ul li a");
+window.addEventListener("scroll", function () {
+  var current = "";
+  sections.forEach(function (section) {
+    var sectionTop = section.offsetTop;
+    var sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 8) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach(function (a) {
+    a.classList.remove("active");
+
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+  });
+});
--- students/varhanov_artem/Exam_js/src/js/script.js
+++ students/varhanov_artem/Exam_js/src/js/script.js
@@ -1,3 +1,35 @@
+function throttle(func, ms) {
+
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+
+
 $(document).ready(function(){
   $('.hero').slick({
     dots: true,
@@ -56,22 +88,97 @@ $(document).ready(function(){
   }); 
 
   $('.button-mobmenu').click(function(event){
-$('.button-mobmenu,.mobile-menu').toggleClass("active")
+  $('.button-mobmenu,.mobile-menu').toggleClass("active")
   });
+  
 });
+
+// $(window).scroll(function(){
+//   if ($(window).scrollTop() > 300) {
+//       $('.header').addClass('sticky');
+//   }
+//   else {
+//       $('.header').removeClass('sticky');
+//   }
+// });
+document.addEventListener('DOMContentLoaded', () =>{
+  const hero = document.querySelector('.hero');
+  const header = document.querySelector('.header');
+  const heroCenter = hero.offsetHeight / 2;
+
+  // const showHeaderThrottle = throttle(() => {
+  //   const scrollTop = window.scrollY;
+  //   (scrollTop >= 100) ? header.classList.add('invisible') : header.classList.remove('invisible');
+  //   (scrollTop >= heroCenter) ? header.classList.add('sticky') : header.classList.remove('sticky');
+  // }, 500)
+  const showHeaderThrottle = () => {
+    const scrollTop = window.scrollY;
+    (scrollTop >= 100) ? header.classList.add('invisible') : header.classList.remove('invisible');
+    (scrollTop >= heroCenter) ? header.classList.add('sticky') : header.classList.remove('sticky');
+  }
+  
+  window.addEventListener('scroll', showHeaderThrottle);
+});
+
+
+
+
+
+
 
 function initMap() {
   const brooklun = {lat: 40.686280255564796, lng: -73.90061989110093};
   const map = new google.maps.Map(document.getElementById('map'), {
     center: brooklun,
-    zoom: 13
+    zoom: 13,
+    styles: [{
+      "elementType": "geometry",
+      "stylers": [{
+          "color": "#e8e8e8"
+      }]
+  },
+ 
+  {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+          "color": "#757575"
+      }]
+  },
+  
+
+]
+
   });
 
   const marker = new google.maps.Marker({
     position: brooklun,
     map,
     title:'Park',
-    icon:'image/Pin.png'
+    icon:'image/Pin.png',
+    
     
   });
 }
+
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li a");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 8) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach((a) => {
+    a.classList.remove("active");
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+      
+    }
+  });
+});
