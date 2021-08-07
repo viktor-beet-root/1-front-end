f11521b0ecfa30a8000a1811f77569854e516aef
Ihor Andrianov <icequize@gmail.com>
2021-08-08 00:44:02 +0300

    added fixes after checking pt2

--- students/andrianov_igor/exam_2/src/js/lib/seeMore.js
+++ students/andrianov_igor/exam_2/src/js/lib/seeMore.js
@@ -5,39 +5,39 @@ function seeMore() {
 <div class="container append">
     <div class="row append__row append__row_adaptive id="seeMoreP">
 
-        <div class="img-list__image-wrapper-a img-list__image-wrapper_left col-3 col-md-6">
+        <div class="col-3 col-md-6 img-list__image-wrapper-a img-list__image-wrapper_left">
             <a class="img-list__image-link-a" href="./images/gallery_2.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_2.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_2.png" alt="gallery">
             </a>
             <a class="img-list__image-link-a" href="./images/gallery_3.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_3.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_3.png" alt="gallery">
             </a>
         </div>
 
-        <div class="img-list__image-wrapper-a img-list__image-wrapper_right col-3 col-md-6">
+        <div class="col-3 col-md-6 img-list__image-wrapper-a img-list__image-wrapper_right">
             <a class="img-list__image-link-a" href="./images/gallery_4.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_4.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_4.png" alt="gallery">
             </a>
             <a class="img-list__image-link-a" href="./images/gallery_5.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_5.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_5.png" alt="gallery">
             </a>
         </div>
 
-        <div class="img-list__image-wrapper-a img-list__image-wrapper_left col-3 col-md-6">
+        <div class="col-3 col-md-6 img-list__image-wrapper-a img-list__image-wrapper_left">
             <a class="img-list__image-link-a" href="./images/gallery_2.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_2.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_2.png" alt="gallery">
             </a>
             <a class="img-list__image-link-a" href="./images/gallery_3.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_3.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_3.png" alt="gallery">
             </a>
         </div>
 
-        <div class="img-list__image-wrapper-a img-list__image-wrapper_right col-3 col-md-6">
+        <div class="col-3 col-md-6 img-list__image-wrapper-a img-list__image-wrapper_right">
             <a class="img-list__image-link-a" href="./images/gallery_4.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_4.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_4.png" alt="gallery">
             </a>
             <a class="img-list__image-link-a" href="./images/gallery_5.png" data-lightbox="lbox">
-                <img class="img-list__img" src="./images/gallery_5.png" alt="gallery">
+                <img class="img-list__img-a" src="./images/gallery_5.png" alt="gallery">
             </a>
         </div>
 
